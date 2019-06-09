18f0f580258bfbaddb789b85b33dcfc37b31942c
Paul Peelen <paul@paulpeelen.com>
2019-06-09 21:23:39 +0200

    Added leaf SQLlite

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", .upToNextMinor(from: "3.3.0")),
+		.package(url: "https://github.com/vapor/fluent-sqlite.git", .upToNextMinor(from: "3.0.0"))
     ],
     targets: [
         .target(name: "App", dependencies: ["Vapor"]),
